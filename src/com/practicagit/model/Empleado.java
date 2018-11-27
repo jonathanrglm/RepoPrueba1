1d149c5ef4a6589e5463384b363bf01333f858c5
jonathanrglm <jonathan_theking@hotmail.com>
2018-11-27 17:45:45 -0600

    Segundo commit, Getters y Setters escritos y Servlet Empleado tamboem

--- src/com/practicagit/model/Empleado.java
+++ src/com/practicagit/model/Empleado.java
@@ -1,5 +1,13 @@
 package com.practicagit.model;
 
 public class Empleado {
-
+	private String idEmpleado;
+	
+	public String getIdEmpleado() {
+		return idEmpleado;
+	}
+	
+	public void setIdEmpleado(String idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
 }
